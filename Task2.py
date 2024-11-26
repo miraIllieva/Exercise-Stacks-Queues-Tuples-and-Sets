4f8ad26e09b77f6ade1eb56d94263fd3bec17064
Myra Ilieva <159176371+miraIllieva@users.noreply.github.com>
2024-11-25 20:07:52 -0500

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,22 +1,23 @@
 seta = set([int(num) for num in input().split()])
-setb = set([int (num) for num in input().split()])
+setb = set([int(num) for num in input().split()])
 
-n = int(nput())
+n = int(input())
 
-for in range(n):
+for i in range(n):
     user_input = input().split()
     command = f'{user_input[0]} {user_input[1]}'
     numbers = [int(number) for number in user_input[2:]]
 
     if command == 'Add First':
         seta.update(numbers)
-    if command == 'Add Second':
+    elif command == 'Add Second':
         setb.update(numbers)
-    if command == 'Remove First':
-        sets.differance_update(numbers)
-    if command = 'Remove Second':
-        setb.differance_update(numbers)
-    elif command = 'Check Subset':
+    elif command == 'Remove First':
+        seta.difference_update(numbers)
+    elif command == 'Remove Second':
+        setb.difference_update(numbers)
+    elif command == 'Check Subset':
         print(seta.issubset(setb) or setb.issubset(seta))
-print(*sorted(seta), sep =', ')
-print(*sorted(setb), sep = ', ')
+
+print(*sorted(seta), sep=', ')
+print(*sorted(setb), sep=', ')
